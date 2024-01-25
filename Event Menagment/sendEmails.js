72ab9ddab96c300e4aa33e69845bfcfdcafb8659
rahmelldili <144826942+RahmellD@users.noreply.github.com>
2024-01-25 14:48:59 +0100

    sendEmails added location

--- Event Menagment/sendEmails.js
+++ Event Menagment/sendEmails.js
@@ -22,11 +22,11 @@ async function sendEmails() {
                     from: 'rameldili1@gmail.com',
                     to: user.email,
                     subject: `Event: ${event.title}`,
-                    text: `Dear ${user.first_name},\n\nEvent Details:\nDescription: ${event.description}\nStart Date: ${event.start_date}\nEnd Date: ${event.end_date}\n\nRegards,\nEvent MEnagment System`
+                    text: `Dear ${user.first_name},\n\nEvent Details:\nDescription: ${event.description}\nStart Date: ${event.start_date}\nEnd Date: ${event.end_date}\nLocation: ${event.location}\n\nRegards,\nEvent MEnagment System`
                 };
 
                 await transporter.sendMail(mailOptions);
-                console.log(`Email sent to ${user.email} for event: ${event.description}`);
+                console.log(`Email sent to ${user.email} for event: ${event.title}`);
             }
         }
     } catch (error) {
